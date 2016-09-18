6c5ad0ca4a4251bd83c28c6fdff85e42de471970
Genesis Font <purewebdev@gmail.com>
2016-09-18 18:43:38 -0400

    adjusted html, improved formatting, displaying db values

--- resources/views/yardsales/index.blade.php
+++ resources/views/yardsales/index.blade.php
@@ -3,43 +3,52 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-12 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">Deltona Yard Sale listings
-                        <button type="button" class="btn btn-primary btn-xs pull-right"><a href="/yard-sales/create">Add
+                        <button type="button" class="btn btn-primary btn-xs pull-right"><a href="/yard-sales/create"
+                                                                                           style="color: #ffffff;">Add
                                 New Listing</a></button>
                     </div>
 
                     <div class="panel-body">
-                        You are logged in! {{ trans('messages.welcome') }} <br/>
-                        <table class="table table-bordered table-striped">
-                            <thead>address</thead>
-                            <thead>city</thead>
-                            <thead>zip</thead>
-                            <thead>privacy</thead>
-                            <thead>type_of_sale</thead>
-                            <thead>start_date</thead>
-                            <thead>end_date</thead>
-                            <thead>payment_types_accepted</thead>
-                            <thead>display_on_map</thead>
-                            <thead>description</thead>
-                            <thead>time</thead>
-                            @foreach ($yardSales as $yardSale)
+                        You are logged in! {{ trans('messages.welcome') }}
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-striped">
+                                <thead>
                                 <tr>
-                                    <td>Yard Sale Address {{ $yardSale->address }}</td>
-                                    <td>Yard Sale Address {{ $yardSale->city }}</td>
-                                    <td>Yard Sale Address {{ $yardSale->zip }}</td>
-                                    <td>Yard Sale Address {{ $yardSale->privacy }}</td>
-                                    <td>Yard Sale Address {{ $yardSale->type_of_sale }}</td>
-                                    <td>Yard Sale Address {{ $yardSale->start_date }}</td>
-                                    <td>Yard Sale Address {{ $yardSale->end_date }}</td>
-                                    <td>Yard Sale Address {{ $yardSale->payment_types_accepted }}</td>
-                                    <td>Yard Sale Address {{ $yardSale->display_on_map }}</td>
-                                    <td>Yard Sale Address {{ $yardSale->description }}</td>
-                                    <td>Yard Sale Address {{ $yardSale->time }}</td>
+                                    <th>Address</th>
+                                    <th>City</th>
+                                    <th>Zip</th>
+                                    <th>Privacy</th>
+                                    <th style="white-space: nowrap">Type of sale</th>
+                                    <th>Start date</th>
+                                    <th>End date</th>
+                                    <th style="white-space: nowrap">Payment types accepted</th>
+                                    <th style="white-space: nowrap">Display on map</th>
+                                    <th>Description</th>
+                                    <th>Time</th>
                                 </tr>
-                            @endforeach
-                        </table>
+                                </thead>
+                                <tbody>
+                                @foreach ($yardSales as $yardSale)
+                                    <tr>
+                                        <td style="white-space: nowrap">{{ $yardSale->address }}</td>
+                                        <td>{{ $yardSale->city }}</td>
+                                        <td>{{ $yardSale->zip }}</td>
+                                        <td>{{ $yardSale->privacy }}</td>
+                                        <td>{{ $yardSale->type_of_sale }}</td>
+                                        <td style="white-space: nowrap">{{ $yardSale->start_date }}</td>
+                                        <td style="white-space: nowrap">{{ $yardSale->end_date }}</td>
+                                        <td>{{ $yardSale->payment_types_accepted }}</td>
+                                        <td>{{ $yardSale->display_on_map }}</td>
+                                        <td>{{ $yardSale->description }}</td>
+                                        <td>{{ $yardSale->time }}</td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
