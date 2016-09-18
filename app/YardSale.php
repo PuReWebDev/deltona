5d54a99df9c131b8d770bd930ab95b881402067e
Genesis Font <purewebdev@gmail.com>
2016-09-18 13:51:26 -0400

    added table and fillable properties

--- app/YardSale.php
+++ app/YardSale.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class YardSale extends Model
 {
-    //
+    /**
+     * @var string $table
+     */
+    protected $table = 'yard_sales';
+
+    /**
+     * @var array $fillable
+     */
+    protected $fillable = [
+        'address',
+        'city',
+        'zip',
+        'privacy',
+        'type_of_sale',
+        'start_date',
+        'end_date',
+        'payment_types_accepted',
+        'display_on_map',
+        'description',
+        'time',
+    ];
 }
